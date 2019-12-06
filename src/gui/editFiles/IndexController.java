87939235cc62bbd21a3bc46d42b6e7975c6003bd
Igor Barbosa Cortez <ibcortez@dac.unicamp.br>
2019-12-06 17:50:17 -0300

    adicinado tratamento de OnChange nos ComboBoxs

--- src/gui/editFiles/IndexController.java
+++ src/gui/editFiles/IndexController.java
@@ -1,21 +1,27 @@
 package gui.editFiles;
 
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import gui.util.Alerts;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import utils.GetDatasJson;
 
 public class IndexController {
 
-	
 	private static FXMLLoader loader;
 	private static AnchorPane anchorPane;
 	private static TextField textFieldRA = null;
@@ -27,6 +33,11 @@ public class IndexController {
 	private static ComboBox<String> comboBoxType = null;
 	private static TextField processNumberTextField = null;
 	private static Button indexButton = null;
+	private static JSONObject jsonResult = null;
+	private static List<String> listLevel = null;
+	private static List<String> listCourse = null;
+	private static List<String> listCategory = null;
+	private static List<String> listType = null;
 
 	@SuppressWarnings("unchecked")
 	public static AnchorPane getIndex() {
@@ -34,48 +45,99 @@ public class IndexController {
 			loader = new FXMLLoader(IndexController.class.getResource("/gui/editFiles/Index.fxml"));
 			anchorPane = loader.load();
 			textFieldRA = (TextField) anchorPane.lookup("#textFieldRA");
-			//Permite que somente números sejam digitados no campo RA
+			// Permite que somente números sejam digitados no campo RA
 			textFieldRA.textProperty().addListener(new ChangeListener<String>() {
-			    @Override
-			    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-			        if (!newValue.matches("\\d*")) {
-			        	textFieldRA.setText(newValue.replaceAll("[^\\d]", ""));
-			        }
-			    }
+				@Override
+				public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+					if (!newValue.matches("\\d*")) {
+						textFieldRA.setText(newValue.replaceAll("[^\\d]", ""));
+					}
+				}
 			});
-		
+
 			textFieldName = (TextField) anchorPane.lookup("#textFieldName");
 			textFieldName.setEditable(false);
 			textFieldCPF = (TextField) anchorPane.lookup("#textFieldCPF");
 			textFieldCPF.setEditable(false);
 			comboBoxLevel = (ComboBox<String>) anchorPane.lookup("#comboBoxLevel");
 			comboBoxLevel.setDisable(true);
+
 			comboBoxCourse = (ComboBox<String>) anchorPane.lookup("#comboBoxCourse");
 			comboBoxCourse.setDisable(true);
+
 			comboBoxCategory = (ComboBox<String>) anchorPane.lookup("#comboBoxCategory");
 			comboBoxCategory.setDisable(true);
+
 			comboBoxType = (ComboBox<String>) anchorPane.lookup("#comboBoxType");
 			comboBoxType.setDisable(true);
 			processNumberTextField = (TextField) anchorPane.lookup("#processNumberTextField");
 			processNumberTextField.setDisable(true);
 			indexButton = (Button) anchorPane.lookup("#indexButton");
 			indexButton.setDisable(true);
-			return anchorPane ;
+
+			return anchorPane;
 		} catch (IOException e) {
-			Alerts.showAlert("Erro", "", "Não foi possível acessar esta opção:"+e.getMessage(),AlertType.ERROR);
+			Alerts.showAlert("Erro", "", "Não foi possível acessar esta opção:" + e.getMessage(), AlertType.ERROR);
 		}
 		return null;
 	}
-	
-	public void onCancelButtonAction(){
+
+	public void onCancelButtonAction() {
 		EditFilesController.setRight(RightController.createPaneImages());
 	}
-	
+
 	public void onButtonConsultAction() {
-		
-		if(textFieldRA.getText().trim().isEmpty()) {
+		if (textFieldRA.getText().trim().isEmpty()) {
 			Alerts.showAlert("Aviso", "", "é necessário informar o número do RA", AlertType.WARNING);
+		} else {
+			jsonResult = new JSONObject(GetDatasJson.getRA(Integer.parseInt(textFieldRA.getText())));
+			if (jsonResult.isEmpty()) {
+				Alerts.showAlert("Aviso", "", "RA não localizado. Favor, verificar", AlertType.WARNING);
+			} else {
+
+				textFieldName.setText(jsonResult.getString("name"));
+				textFieldCPF.setText(jsonResult.getString("CPF"));
+
+				listLevel = new ArrayList<String>();
+				comboBoxLevel.setDisable(false);
+				comboBoxLevel.getItems().addAll(addItems(listLevel, "level"));
+
+			}
 		}
+	}
+
+	public void comboBoxLevelOnAction() {
+		if (!comboBoxLevel.getSelectionModel().isEmpty()) {
+			comboBoxCourse.setDisable(false);
+			listCourse = new ArrayList<String>();
+			comboBoxCourse.getItems().addAll(addItems(listCourse, "course"));
+		}
+	}
+
+	public void comboBoxCourseOnAction() {
+		if (!comboBoxCourse.getSelectionModel().isEmpty()) {
+			comboBoxCategory.setDisable(false);
+			listCategory = new ArrayList<String>();
+			comboBoxCategory.getItems().addAll(addItems(listCategory, "category"));
+		}
+	}
+
+	public void comboBoxCategoryOnAction() {
+		if (!comboBoxCategory.getSelectionModel().isEmpty()) {
+			comboBoxType.setDisable(false);
+			listType = new ArrayList<String>();
+			comboBoxType.getItems().addAll(addItems(listType, "type"));
+		}
+	}
+
+	private static ObservableList<String> addItems(List<String> list, String field) {
+		JSONArray jsonArray = null;
+		jsonArray = new JSONArray(jsonResult.get(field).toString());
+		for (int i = 0; i < jsonArray.length(); i++) {
+			list.add(jsonArray.getString(i));
+		}
+
+		return FXCollections.observableArrayList(listLevel);
 	}
 
 }
---
+++ src/utils/GetDatasJson.java
@@ -0,0 +1,34 @@
+package utils;
+
+import org.json.JSONObject;
+
+public class GetDatasJson {
+	private static JSONObject jsonRA = new JSONObject();
+
+
+	public static String getRA(Integer ra) {
+		switch (ra) {
+		case 123456:
+			jsonRA.put("ra", "123456");
+			jsonRA.put("name", "Carlos Adão");
+			jsonRA.put("CPF", "902.759.410-43");
+			jsonRA.put("level", "[graduação, pos-graduação]");
+			jsonRA.put("course", "[matematica,Economia,fisica]");
+			jsonRA.put("category", "[documentos acadêmicos,documntos pessoais]");
+			jsonRA.put("type", "[RG,Certidão de nascimento, Histórico escolar]");
+			return jsonRA.toString();
+		case 654321:
+			jsonRA.put("ra", "123456");
+			jsonRA.put("name", "Maria José");
+			jsonRA.put("CPF", "516.323.970-80");
+			jsonRA.put("level", "[graduação, mestrado]");
+			jsonRA.put("course", "[matematica,Economia,fisica]");
+			jsonRA.put("category", "[documentos acadêmicos,documntos pessoais]");
+			jsonRA.put("type", "[RG,Certidão de nascimento,comprovante de residência]");
+			return jsonRA.toString();
+		default:
+			return jsonRA.toString();
+		}
+	}
+
+}
